43acf6ee42e789b8c2a2c578ea442494cd65ff1f
Ronny <ronnyangelo.freites@gmail.com>
2020-09-16 01:37:36 -0600

    add base layout

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,18 +4,4 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-import React from 'react'
-
-import BaseLayout from './src/components/base-layout'
-
-// eslint-disable-next-line no-unused-vars
-export const onRenderBody = ({ setBodyAttributes }, pluginOptions) => {
-  // setBodyAttributes({
-  //   className: 'bg-gray-lightBackground',
-  // })
-}
-
-// Wraps every page in a component
-export const wrapPageElement = ({ element, props }) => {
-  return <BaseLayout {...props}>{element}</BaseLayout>
-}
+// You can delete this file if you're not using it
