475525faf88a59284982e0b4ac05d6041149fe89
Ronny <ronnyangelo.freites@gmail.com>
2020-09-17 13:11:29 -0600

    add styles intro section

--- src/components/introduction-title-text/index.tsx
+++ src/components/introduction-title-text/index.tsx
@@ -6,6 +6,8 @@ import ParagraphText from '../paragraph-text'
 const TextContainer = styled.div`
   @media (min-width: 1024px) {
     max-width: 16rem;
+    margin-left: 90px;
+    margin-top: 5px;
   }
 `
 
@@ -15,10 +17,21 @@ const BoxDecoration = styled.div`
     position: absolute;
     top: 0;
     width: 5px;
-    right: 90px;
+    right: 70px;
     height: 166px;
     background-color: #fff;
     display: block;
+    &:before {
+      content: '';
+      background-image: url("data:image/svg+xml,%3Csvg width='129' height='130' viewBox='0 0 129 130' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cg style='mix-blend-mode:overlay'%3E%3Ccircle cx='3.5' cy='3' r='3' fill='white'/%3E%3Ccircle cx='3.5' cy='34' r='3' fill='white'/%3E%3Ccircle cx='3.5' cy='65' r='3' fill='white'/%3E%3Ccircle cx='3.5' cy='96' r='3' fill='white'/%3E%3Ccircle cx='3.5' cy='127' r='3' fill='white'/%3E%3Ccircle cx='34.5' cy='3' r='3' fill='white'/%3E%3Ccircle cx='34.5' cy='34' r='3' fill='white'/%3E%3Ccircle cx='34.5' cy='65' r='3' fill='white'/%3E%3Ccircle cx='34.5' cy='96' r='3' fill='white'/%3E%3Ccircle cx='34.5' cy='127' r='3' fill='white'/%3E%3Ccircle cx='65.5' cy='3' r='3' fill='white'/%3E%3Ccircle cx='65.5' cy='34' r='3' fill='white'/%3E%3Ccircle cx='65.5' cy='65' r='3' fill='white'/%3E%3Ccircle cx='65.5' cy='96' r='3' fill='white'/%3E%3Ccircle cx='65.5' cy='127' r='3' fill='white'/%3E%3Ccircle cx='96.5' cy='3' r='3' fill='white'/%3E%3Ccircle cx='96.5' cy='34' r='3' fill='white'/%3E%3Ccircle cx='96.5' cy='65' r='3' fill='white'/%3E%3Ccircle cx='96.5' cy='96' r='3' fill='white'/%3E%3Ccircle cx='96.5' cy='127' r='3' fill='white'/%3E%3Ccircle cx='125.5' cy='3' r='3' fill='white'/%3E%3Ccircle cx='125.5' cy='34' r='3' fill='white'/%3E%3Ccircle cx='125.5' cy='65' r='3' fill='white'/%3E%3Ccircle cx='125.5' cy='96' r='3' fill='white'/%3E%3Ccircle cx='125.5' cy='127' r='3' fill='white'/%3E%3C/g%3E%3C/svg%3E%0A");
+      width: 128px;
+      height: 130px;
+      right: 106px;
+      position: absolute;
+      top: 169px;
+      background-repeat: no-repeat;
+      background-size: contain;
+    }
   }
 `
 
@@ -36,9 +49,21 @@ interface IntroductionTitleTextProps {
   }
 }
 
+const Container = styled.div`
+  @media (min-width: 1024px) {
+    margin-top: 145px;
+  }
+`
+
+const TextWrapper = styled.div`
+  @media (min-width: 1024px) {
+    margin-left: 20px;
+  }
+`
+
 const IntroductionTitleText = (props: IntroductionTitleTextProps) => {
   return (
-    <div className="mt-10 lg:mt-24 flex flex-col lg:grid lg:grid-cols-2 pb-10 lg:pb-64">
+    <Container className="mt-10 flex flex-col lg:grid lg:grid-cols-2 pb-10 lg:pb-64">
       <div className="lg:flex items-start lg:relative">
         <BoxDecoration />
         <TextContainer>
@@ -49,7 +74,7 @@ const IntroductionTitleText = (props: IntroductionTitleTextProps) => {
           )}
         </TextContainer>
       </div>
-      <div className="mt-8 lg:mt-0">
+      <TextWrapper className="mt-8 lg:mt-0">
         <p className="font-poppins text-xl text-white text-center lg:text-left lg:max-w-xl">
           {props.text &&
             props.text?.raw?.children?.map(p => (
@@ -60,8 +85,8 @@ const IntroductionTitleText = (props: IntroductionTitleTextProps) => {
               />
             ))}
         </p>
-      </div>
-    </div>
+      </TextWrapper>
+    </Container>
   )
 }
 
